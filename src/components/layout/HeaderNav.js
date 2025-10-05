a1582cc216ab7894a2a09c8d9409d327b7627690
tarikopenclassrooms <maarijtarik@hotmail.com>
2025-10-06 00:16:54 +0200

    Barra de navegación entre rutas

--- src/components/layout/HeaderNav.js
+++ src/components/layout/HeaderNav.js
@@ -1,13 +1,32 @@
 
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const HeaderNav = () => {
   return (
     <header>
-      <h1>Víctor Robles WEB</h1>
+      <div className="logo">
+        <span>V</span>
+        <h3>Víctor Robles WEB</h3>
+      </div>
+      
       <nav>
         <ul>
-          <li></li>
+          <li>
+            <NavLink to="/inicio"> Inicio </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portafolio"> Portafolio </NavLink>
+          </li>
+          <li>
+            <NavLink to="/servicios"> Servicios </NavLink>
+          </li>
+          <li>
+            <NavLink to="/curriculum"> Curriculum </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacto"> Contacto </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
