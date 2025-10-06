7f9caf23050a78e640dc3562f9ff0e8c53749ad6
tarikopenclassrooms <maarijtarik@hotmail.com>
2025-10-07 01:43:21 +0200

    Crear array objetos de proyectos

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { trabajos } from "../data/trabajos";
 
 export const Portafolio = () => {
   return (
     <div className="page">
       <h1 className="heading"> Portafolio </h1>
+
+       {
+        trabajos.map(trabajo => {
+          return( <article>
+            <h2>{trabajo.nombre}</h2>
+          </article>)
+         
+        }) 
+       }
+
     </div>
   );
 };
---
+++ src/data/trabajos.js
@@ -0,0 +1,46 @@
+
+export const trabajos = [
+        {
+                'id': 'victorroblesweb',
+                'nombre': 'Victor Robles Web',
+                'url': 'victorroblesweb',
+                'tecnologías': 'HTML, CSS, JS, PHP, MySQL',
+                'categorías': 'Desarrollo web'
+        },
+        {
+                'id': 'facebook',
+                'nombre': 'Facebook',
+                'url': 'facebook.com',
+                'tecnologías': 'React, Node, Mongo',
+                'categorías': 'Desarrollo web'
+        },
+        {
+                'id': 'twitter',
+                'nombre': 'Twitter',
+                'url': 'twitter.com',
+                'tecnologías': 'HTML, CSS',
+                'categorías': 'Maquetación web'
+        },
+        {
+                'id': 'linkedin',
+                'nombre': 'Linkedin',
+                'url': 'linkedin.com',
+                'tecnologías': 'HTML, CSS, JS, PHP, Larvel',
+                'categorías': 'Desarrollo web'
+        },
+        {
+                'id': 'udemy',
+                'nombre': 'Udemy',
+                'url': 'udemy.com/user/victor-robles-2/',
+                'tecnologías': 'Mean stack',
+                'categorías': 'Desarrollo web'
+        },
+        {
+                'id': 'youtube',
+                'nombre': 'Youtube',
+                'url': 'youtube.com/c/VictorRoblesWeb',
+                'tecnologías': 'JS, React, PHP, Symfony',
+                'categorías': 'Desarrollo web'
+        },
+        
+]
