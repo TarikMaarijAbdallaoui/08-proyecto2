9341ba19eada56bb950c9e7b146e60541fcdb857
tarikopenclassrooms <maarijtarik@hotmail.com>
2025-10-07 16:27:46 +0200

    Mostrar imágenes y maquetación

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -7,17 +7,22 @@ export const Portafolio = () => {
     <div className="page">
       <h1 className="heading"> Portafolio </h1>
 
-       {
-        trabajos.map(trabajo => {
-          return( <article key={trabajo.id}>
-            <span>{trabajo.categorías}</span>
-            <h2><Link to={"/proyecto/" + trabajo.id}>{trabajo.nombre}</Link></h2>
-            <h3>{trabajo.tecnologías}</h3>
-          </article>)
-         
-        }) 
-       }
-
+      <section className="works">
+        {trabajos.map((trabajo) => {
+          return (
+            <article key={trabajo.id} className="work-item">
+              <div className="mask">
+                <img src={"/images/" + trabajo.id + ".png"} />
+              </div>
+              <span>{trabajo.categorías}</span>
+              <h2>
+                <Link to={"/proyecto/" + trabajo.id}>{trabajo.nombre}</Link>
+              </h2>
+              <h3>{trabajo.tecnologías}</h3>
+            </article>
+          );
+        })}
+      </section>
     </div>
   );
 };
